97ade7255fbc8337f2cc5a286c56a41fad2f4687
Josué Salazaku <josuesalazaku@gmail.com>
2024-05-18 22:46:57 +0200

    display questions

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,43 @@
-import axios from "axios";
+import axios from 'axios';
 
-const questionsSection = document.getElementById('questions-section');
-const questionsBlock = document.getElementById('questions-block');
-const submitButton = document.getElementById('submit-btn');
-const skipButton = document.getElementById('skip-btn');
+const questionsSection = document.getElementById('questions-section') as HTMLElement;
+const questionsBlock = document.getElementById('questions-block') as HTMLElement;
+const submitButton = document.getElementById('submit-btn') as HTMLButtonElement;
+const skipButton = document.getElementById('skip-btn') as HTMLButtonElement;
+
+
 
 export const fetchQuestions = async () => { 
-    const response = await axios.get('http://localhost:8080/api/questions');
-    const data = await response.data;
-    console.log(data);
-    return data.results;
+    try {
+        const response = await axios.get('http://localhost:8080/api/questions');
+        const data = response.data;
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error("Error fetching questions:", error);
+    }   
 }
 
+const showQuestions = async () => {
+    const questions = await fetchQuestions();
+    
+    try {
+        questions.forEach((question: any) => {
+            const questionDiv = document.createElement('div');
+
+            const questionCheckbox = document.createElement('input');
+            questionCheckbox.type = 'checkbox';
+            questionDiv.appendChild(questionCheckbox);
+
+            const questionsParagraph = document.createElement('p');
+            questionsParagraph.textContent = question.question_text;
+            questionDiv.appendChild(questionsParagraph);
+
+            questionsBlock.appendChild(questionDiv);
+        })
+    } catch (error) {
+        console.error("Error showing questions:", error);
+    }
+}
+
+showQuestions()
